0bb1ce5635f08620c72d18680b039f3dfca5c256
yws233 <yws_ssh@163.com>
2017-12-15 00:30:31 +0800

    登录页面优化

--- WARMLOVE/js/loginPage.js
+++ WARMLOVE/js/loginPage.js
@@ -7,6 +7,15 @@ function changeBG() {
         image[1] = "url(../img/contextbg2.jpg)";
         image[2] = "url(../img/contextbg3.jpg)";
         image[3] = "url(../img/contextbg4.jpg)";
-        var rand = Math.floor(Math.random() * 4);
-        document.getElementById("context").style.background = image[rand];
+        image[3] = "url(../img/contextbg5.jpg)";
+        var rand = Math.floor(Math.random() * 5);
+        document.getElementById("context").style.backgroundImage = image[rand];
 }
+/*function changeColor() {
+    document.getElementById("text-nav").style.color = "#4F82F9";
+}
+function changeColor2() {
+    document.getElementById("text-nav2").style.color = "#4F82F9";
+}*/
+
+
